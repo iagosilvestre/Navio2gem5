291ec55557d0254a45ac2863acf233d2ebc04cb7
iagosilvestre <32106546+iagosilvestre@users.noreply.github.com>
2019-05-30 17:00:43 -0300

    printing matrix

--- C++/Examples/ControlModule/c_control_lqrArthur_vel.c
+++ C++/Examples/ControlModule/c_control_lqrArthur_vel.c
@@ -189,7 +189,9 @@ pv_type_actuation c_control_lqrArthur_vel_controller(pv_msg_input inputData){
 
 	printf("control output numCols = %d\n",control_output.numCols);
 	printf("control output numRows = %d\n",control_output.numRows);
-	printf("control output[0] = %d\n",control_output.pData);
+	for (i = 0; i < 3; i++)
+	      printf("%d %d ", control_output.pData[i][0], *(*(control_output.pData + i) + j));
+	    printf("\n");
 	//
 	//The result must be in a struct pv_msg_io_actuation
 	actuation_signals.escRightNewtons= (float)control_output.pData[0];
