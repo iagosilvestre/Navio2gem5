fd739390f930ad428773467ce0b93d1e9848f86b
iagosilvestre <32106546+iagosilvestre@users.noreply.github.com>
2019-05-30 17:58:46 -0300

    missing ;

--- C++/Examples/ControlModule/c_control_lqrArthur_vel.c
+++ C++/Examples/ControlModule/c_control_lqrArthur_vel.c
@@ -194,7 +194,7 @@ pv_type_actuation c_control_lqrArthur_vel_controller(pv_msg_input inputData){
 	printf("%f\n",*(*(&control_output.pData + 0) + 2));
 	printf("%f\n",*(*(&control_output.pData + 0) + 3));
 
-	printf("\n----------------------------------------\n")
+	printf("\n----------------------------------------\n");
 	printf("equilibrium_control numCols = %d\n",equilibrium_control.numCols);
 	printf("equilibrium_control numRows = %d\n",equilibrium_control.numRows);
 //	printf("%f\n",*(*(&equilibrium_control.pData + 0) + 0));
