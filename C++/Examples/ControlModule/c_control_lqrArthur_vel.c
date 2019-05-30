9080182f5156042b7c5fabb06552d543a8061f24
iagosilvestre <32106546+iagosilvestre@users.noreply.github.com>
2019-05-30 16:54:38 -0300

    trying to print matrix

--- C++/Examples/ControlModule/c_control_lqrArthur_vel.c
+++ C++/Examples/ControlModule/c_control_lqrArthur_vel.c
@@ -189,6 +189,7 @@ pv_type_actuation c_control_lqrArthur_vel_controller(pv_msg_input inputData){
 
 	printf("control output numCols = %d\n",control_output.numCols);
 	printf("control output numRows = %d\n",control_output.numRows);
+	printf("control output[0] = %d\n",control_output.pData);
 	//
 	//The result must be in a struct pv_msg_io_actuation
 	actuation_signals.escRightNewtons= (float)control_output.pData[0];
