7b87b4747bcbc0b478c097033109cf6927e86c5c
Iago Lab Provant <iagosilvestrehp@hotmail.com>
2019-11-27 15:26:54 -0300

    testes gem5

--- C++/Examples/ControlModule/controlM.cpp
+++ C++/Examples/ControlModule/controlM.cpp
@@ -80,25 +80,21 @@ void module_co_run()
 	oControlOutputData.actuation.escLeftNewtons = 10.2799;
 
   for( int k = 0; k < 100; k += 1 ){
+	iInputData.position.x = pos_ref.x;
+	iInputData.position.y = pos_ref.y;
+	iInputData.position.z = pos_ref.z;
+	iInputData.position.dotX = pos_ref.dotX;
+	iInputData.position.dotY = pos_ref.dotY;
+	iInputData.position.dotZ = pos_ref.dotZ;
+
+	pos_ref.x = rand() % 10 + 1;
+	pos_ref.y = rand() % 10 + 1;
+	pos_ref.z = rand() % 10 + 1;
+	pos_ref.dotX = rand() % 10 + 1;
+	pos_ref.dotY = rand() % 10 + 1;
+	pos_ref.dotZ = rand() % 10 + 1;
+
 	  gettimeofday(&t0, NULL);
-
-	  if ((k%10)==0){
-		pos_ref.x += .1;
-		pos_ref.y += .1;
-		pos_ref.z += .1;
-		pos_ref.dotX += .1;
-		pos_ref.dotY += .1;
-		pos_ref.dotZ += .1;
-	  }
-	  else if ((k%3)==0){
-		iInputData.position.x = pos_ref.x;
-		iInputData.position.y = pos_ref.y;
-		iInputData.position.z = pos_ref.z;
-		iInputData.position.dotX = pos_ref.dotX;
-		iInputData.position.dotY = pos_ref.dotY;
-		iInputData.position.dotZ = pos_ref.dotZ;
-	  	  }
-
 	/* Passa os valores davariavel compartilha para a variavel iInputData */
 	if (1)
 	{
@@ -152,6 +148,6 @@ int main()
 		usleep(200000);
 		gettimeofday(&t1, NULL);
 		timersub(&t1, &t0, &debug);
-		printf("200.000 us:%lu\n",debug.tv_usec);
+		printf("\n200.000 us:%lu\n",debug.tv_usec);
     return 0;
 }
