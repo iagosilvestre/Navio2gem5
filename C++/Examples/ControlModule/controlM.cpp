1ef6ee614c653a69d7f0802763364d3471188103
Iago Lab Provant <iagosilvestrehp@hotmail.com>
2019-12-03 10:27:32 -0300

    removed printfs

--- C++/Examples/ControlModule/controlM.cpp
+++ C++/Examples/ControlModule/controlM.cpp
@@ -108,10 +108,10 @@ void module_co_run()
 		gettimeofday(&t1, NULL);
 		timersub(&t1, &t0, &dtCon);
 		controlData.push_back(dtCon.tv_usec);
-		printf("\n escRightNewtons = %f\n",oControlOutputData.actuation.escRightNewtons);
-		printf("\n escLeftNewtons = %f\n",oControlOutputData.actuation.escLeftNewtons);
-		printf("\n servoRight = %f\n",oControlOutputData.actuation.servoRight);
-		printf("\n servoLeft = %f\n",oControlOutputData.actuation.servoLeft);
+//		printf("\n escRightNewtons = %f\n",oControlOutputData.actuation.escRightNewtons);
+//		printf("\n escLeftNewtons = %f\n",oControlOutputData.actuation.escLeftNewtons);
+//		printf("\n servoRight = %f\n",oControlOutputData.actuation.servoRight);
+//		printf("\n servoLeft = %f\n",oControlOutputData.actuation.servoLeft);
 		//oControlOutputData.actuation = c_control_lqrArthur_controller(iInputData);
 	}
 
