af5fb69d83a727a09d0ef26803f27129f3c112d5
iago notebook <iagosilvestrehp@hotmail.com>
2021-06-24 16:38:00 -0300

    lqr teste becker'

---
+++ C++/Examples/vant2_lqr/vant2lqr.cpp
@@ -0,0 +1,47 @@
+#include "Icontroller.hpp"
+#include <iostream>
+#include <Eigen/Eigen>
+#include "include/Sensor.h"
+#include "include/SensorArray.h"
+#include "math.h"
+#include "lqr.cpp"
+#include <algorithm>
+#include <chrono>
+#include <vector>
+#include <memory>
+#include <string>
+//#include "m5op.h"
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>
+
+
+
+int main()
+	{
+	std::vector<int> controlData;
+	unsigned long int auxCount=0;
+
+	simulator_msgs::SensorArray arraymsg;
+	std::vector<double> out;
+	std::vector<double> xref;
+	std::vector<double> error;
+	std::vector<double> x;
+	int k=0;
+
+		teste* control = new teste();
+
+		control->config();
+
+		while(k<100){
+
+		out=control->execute(arraymsg);
+		//std::cout << " Out:";	
+		//for (auto i = out.begin(); i != out.end(); ++i)
+    		//	std::cout << *i << ' ';
+		k++;
+		//std::cout << " \n";
+		}
+
+		return 0;
+	}
