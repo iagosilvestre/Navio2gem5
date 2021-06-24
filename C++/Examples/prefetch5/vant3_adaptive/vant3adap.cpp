fd1ad9871649f494fd096c75fdd8904f23aabc18
iago notebook <iagosilvestrehp@hotmail.com>
2021-06-24 11:35:47 -0300

    compile C for control.txt binaries

--- C++/Examples/prefetch5/vant3_adaptive/vant3adap.cpp
+++ C++/Examples/prefetch5/vant3_adaptive/vant3adap.cpp
@@ -19,7 +19,7 @@
 
 int main()
 	{
-	auto all = std::chrono::high_resolution_clock::now();
+	//	auto all = std::chrono::high_resolution_clock::now();
 	std::vector<int> controlData;
 	unsigned long int auxCount=0;
 
@@ -45,26 +45,25 @@ int main()
 //		msgstates = arraymsg.values.at(0);
 
 		while(k<100){
-		auto start = std::chrono::high_resolution_clock::now();
+		//auto start = std::chrono::high_resolution_clock::now();
 
 		out=control->execute(arraymsg);
 
-		auto elapsed = std::chrono::high_resolution_clock::now() - start;
-		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-		controlData.push_back(microseconds);
+//		auto elapsed = std::chrono::high_resolution_clock::now() - start;
+//		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+//		controlData.push_back(microseconds);
 		k++;
 		}
-		auto elapsed = std::chrono::high_resolution_clock::now() - all;
-		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-		controlData.push_back(microseconds);
-			FILE *fCON = fopen("control.txt", "w");
-				fprintf(fCON, "count;dtCon\n");
-				fclose(fCON);
-				for (std::vector<int>::iterator it = controlData.begin() ; it != controlData.end(); ++it){
-					auxCount++;
-					FILE *fCON = fopen("control.txt", "a");
-					fprintf(fCON, "%d;%lu\n",auxCount,*it);
-					fclose(fCON);
-				}
+//		auto elapsed = std::chrono::high_resolution_clock::now() - all;
+//		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
+//		controlData.push_back(microseconds);
+//			FILE *fCON = fopen("control.txt", "w");
+//				fprintf(fCON, "count;dtCon\n");
+//				fclose(fCON);
+//				for (std::vector<int>::iterator it = controlData.begin() ; it != controlData.end(); ++it){
+//					FILE *fCON = fopen("control.txt", "a");
+//					fprintf(fCON, "%d;%lu\n",auxCount,*it);
+//					fclose(fCON);
+//				}
 		return 0;
 	}
