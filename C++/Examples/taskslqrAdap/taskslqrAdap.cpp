9a9c96f3918892c48af74b38d3aaf4a831e45681
iago notebook <iagosilvestrehp@hotmail.com>
2022-03-11 03:10:41 -0300

    utime

--- C++/Examples/taskslqrAdap/taskslqrAdap.cpp
+++ C++/Examples/taskslqrAdap/taskslqrAdap.cpp
@@ -120,15 +120,18 @@ void* lqr(void *)
 {
 	rusage ru_adap;
 	rusage ru_adapO;
+	rusage ru_program;
 	stick_this_thread_to_core(0);
 	std::vector<int> controlData;
 	std::vector<long> vincData;
 	std::vector<long> invincData;
+	std::vector<long> secData;
+	std::vector<long> usecData;
 	unsigned long int auxCount=0;	
 	long vinc=0;
 	long invinc=0;
 
-	getrusage(RUSAGE_SELF,&ru_adapO);	
+	getrusage(RUSAGE_THREAD,&ru_adapO);	
 	while(count<100){
 		auto start = std::chrono::high_resolution_clock::now();
 		//m5_reset_stats(0,0);
@@ -140,6 +143,9 @@ void* lqr(void *)
 		invinc=ru_adap.ru_nivcsw-ru_adapO.ru_nivcsw;
 		invincData.push_back(invinc);
 		getrusage(RUSAGE_THREAD,&ru_adapO);
+		getrusage(RUSAGE_SELF,&ru_program);
+		secData.push_back(ru_program.ru_utime.tv_sec);
+		usecData.push_back(ru_program.ru_utime.tv_usec);
 		//printf("preempt count is %d\n",current_thread_info()->preempt_count);
 		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
 		controlData.push_back(microseconds);
@@ -152,7 +158,7 @@ void* lqr(void *)
 			for (std::vector<int>::iterator it = controlData.begin() ; it != controlData.end(); ++it){
 				auxCount++;
 				FILE *fCON = fopen("lqr.txt", "a");
-				fprintf(fCON, "%d;%lu;%lu\n",auxCount,*it,invincData[auxCount-1]);
+				fprintf(fCON, "%d;%lu;%lu;%lu;%lu\n",auxCount,*it,invincData[auxCount-1],secData[auxCount-1],usecData[auxCount-1]);
 				fclose(fCON);
 			}
 	pthread_exit(NULL);	/* terminate the thread */
