e78f4dead03d4f91a53966cdb7c99d2f3a3d9aa6
iago notebook <iagosilvestrehp@hotmail.com>
2022-03-11 03:26:28 -0300

    teste usec

--- C++/Examples/taskslqrAdap/taskslqrAdap.cpp
+++ C++/Examples/taskslqrAdap/taskslqrAdap.cpp
@@ -74,15 +74,20 @@ void *adap( void *ptr )
 {	
 	rusage ru_adap;
 	rusage ru_adapO;
+	rusage ru_program;
+	rusage ru_programStart;
 	stick_this_thread_to_core(0);
 	std::vector<int> controlData;
 	std::vector<long> vincData;
 	std::vector<long> invincData;
+	std::vector<long> secData;
+	std::vector<long> usecData;
 	unsigned long int auxCount=0;	
 	long vinc=0;
 	long invinc=0;
 
-	getrusage(RUSAGE_SELF,&ru_adapO);	
+	getrusage(RUSAGE_SELF,&ru_programStart);	
+	getrusage(RUSAGE_THREAD,&ru_adapO);
 	while(count<100){
 	count++;
    //m5_reset_stats(0,0);
@@ -97,6 +102,9 @@ void *adap( void *ptr )
 		invinc=ru_adap.ru_nivcsw-ru_adapO.ru_nivcsw;
 		invincData.push_back(invinc);
 		getrusage(RUSAGE_THREAD,&ru_adapO);
+		getrusage(RUSAGE_SELF,&ru_program);
+		secData.push_back(ru_program.ru_utime.tv_sec);
+		usecData.push_back(ru_program.ru_utime.tv_usec);
 		//printf("Adaptive Thread voluntary context switches :%ld\n",ru_adap.ru_nvcsw);
 		//printf("Adaptive Thread involuntary context switches :%ld\n",ru_adap.ru_nivcsw);
 		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
@@ -106,12 +114,12 @@ void *adap( void *ptr )
    //m5_dump_stats(0,0);
    }
 	FILE *fCON = fopen("adap.txt", "w");
-				fprintf(fCON, "count;dtCon\n");
+				fprintf(fCON, "count;dtCon;%lu;%lu\n",ru_programStart.ru_utime.tv_usec,ru_programStart.ru_utime.tv_usec);
 				fclose(fCON);
 				for (std::vector<int>::iterator it = controlData.begin() ; it != controlData.end(); ++it){
 					auxCount++;
 					FILE *fCON = fopen("adap.txt", "a");
-					fprintf(fCON, "%d;%lu;%lu\n",auxCount,*it,invincData[auxCount-1]);
+					fprintf(fCON, "%d;%lu;%lu;%lu;%lu\n",auxCount,*it,invincData[auxCount-1],secData[auxCount-1],usecData[auxCount-1]);
 					fclose(fCON);
 				}
    pthread_exit(NULL);				/* terminate the thread */
@@ -120,13 +128,10 @@ void* lqr(void *)
 {
 	rusage ru_adap;
 	rusage ru_adapO;
-	rusage ru_program;
 	stick_this_thread_to_core(0);
 	std::vector<int> controlData;
 	std::vector<long> vincData;
 	std::vector<long> invincData;
-	std::vector<long> secData;
-	std::vector<long> usecData;
 	unsigned long int auxCount=0;	
 	long vinc=0;
 	long invinc=0;
@@ -143,9 +148,6 @@ void* lqr(void *)
 		invinc=ru_adap.ru_nivcsw-ru_adapO.ru_nivcsw;
 		invincData.push_back(invinc);
 		getrusage(RUSAGE_THREAD,&ru_adapO);
-		getrusage(RUSAGE_SELF,&ru_program);
-		secData.push_back(ru_program.ru_utime.tv_sec);
-		usecData.push_back(ru_program.ru_utime.tv_usec);
 		//printf("preempt count is %d\n",current_thread_info()->preempt_count);
 		long long microseconds = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
 		controlData.push_back(microseconds);
@@ -158,7 +160,7 @@ void* lqr(void *)
 			for (std::vector<int>::iterator it = controlData.begin() ; it != controlData.end(); ++it){
 				auxCount++;
 				FILE *fCON = fopen("lqr.txt", "a");
-				fprintf(fCON, "%d;%lu;%lu;%lu;%lu\n",auxCount,*it,invincData[auxCount-1],secData[auxCount-1],usecData[auxCount-1]);
+				fprintf(fCON, "%d;%lu;%lu\n",auxCount,*it,invincData[auxCount-1]);
 				fclose(fCON);
 			}
 	pthread_exit(NULL);	/* terminate the thread */
